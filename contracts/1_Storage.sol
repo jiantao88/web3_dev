3e3646558c08baf51aa0bd337253301041eea018
jiantao88 <a_tao123@163.com>
2024-06-17 17:13:51 +0800

    StorageFactory

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -7,9 +7,9 @@ pragma solidity >=0.8.2 <0.9.0;
  * @dev Store & retrieve value in a variable
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
-contract Storage {
+contract SimpleStorage {
 
-     uint256 favoriteNumber;
+    uint256 favoriteNumber;
 
     struct People {
         uint256 favoriteNumber;
@@ -20,7 +20,7 @@ contract Storage {
 
     mapping(string => uint256) public nameToFavoriteNumber;
 
-    function store(uint256 _favoriteNumber) public {
+    function store(uint256 _favoriteNumber) public virtual {
         favoriteNumber = _favoriteNumber;
     }
     
---
+++ contracts/StorageFactory.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "./1_Storage.sol";
+
+contract StorageFactory {
+
+    SimpleStorage[] public simpleStorageArray;
+    
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address 
+        // ABI 
+        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+    
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+}
